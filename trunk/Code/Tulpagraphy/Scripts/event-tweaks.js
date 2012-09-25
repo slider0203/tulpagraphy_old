23f598a6c4691cde132f33a85612b3ba2b4df9fe
antman0706@gmail.com <antman0706@gmail.com@8a0912a9-99a5-8134-f182-9c2cec53b616>
2012-09-25 04:04:51 +0000

    Cleaned up (Reshaper)

--- trunk/Code/Tulpagraphy/Scripts/event-tweaks.js
+++ trunk/Code/Tulpagraphy/Scripts/event-tweaks.js
@@ -1,24 +1,23 @@
-﻿$(function () {
-	var leftButtonDown = false;
+﻿$(function() {
+    var leftButtonDown = false;
 
-	window.tweakMouseEvent = function (e) {
-		// Check from jQuery UI for IE versions < 9
-		if ($.browser.msie && !(document.documentMode >= 9) && !event.button) {
-			leftButtonDown = false;
-		}
+    window.tweakMouseEvent = function(e) {
+        // Check from jQuery UI for IE versions < 9
+        if ($.browser.msie && !(document.documentMode >= 9) && !event.button) {
+            leftButtonDown = false;
+        }
 
-		// If left button is not set, set which to 0
-		// This indicates no buttons pressed
-		if (e.which === 1 && !leftButtonDown) e.which = 0;
-	}
+        // If left button is not set, set which to 0
+        // This indicates no buttons pressed
+        if (e.which === 1 && !leftButtonDown) e.which = 0;
+    };
+    $(document).mousedown(function(e) {
+        // Left mouse button was pressed, set flag
+        if (e.which === 1) leftButtonDown = true;
+    });
 
-	$(document).mousedown(function (e) {
-		// Left mouse button was pressed, set flag
-		if (e.which === 1) leftButtonDown = true;
-	});
-
-	$(document).mouseup(function (e) {
-		// Left mouse button was released, clear flag
-		if (e.which === 1) leftButtonDown = false;
-	});
+    $(document).mouseup(function(e) {
+        // Left mouse button was released, clear flag
+        if (e.which === 1) leftButtonDown = false;
+    });
 });
